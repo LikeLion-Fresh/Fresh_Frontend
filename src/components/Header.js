fefa17655f9733b58c60777bc97fd6681467f661
frontjsx <tjrbs33@gmail.com>
2024-08-05 19:01:04 +0900

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 /* src/components/Header.js */
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleSubscribeClick = () => {
+    navigate("/membership");
+  };
+
   return (
     <div className="header">
       <div className="logo">Fresh Time</div>
-      <div className="search-bar">
-        <input type="text" placeholder="Search..." />
+      <div className="search-bar-container">
+        <input type="text" placeholder="Search..." className="search-bar" />
+      </div>
+      <div className="header-right">
+        <button onClick={handleSubscribeClick} className="subscribe-button">
+          구독하기
+        </button>
+        <FontAwesomeIcon icon={faBell} className="notification-icon" />
       </div>
     </div>
   );
